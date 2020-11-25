6b95363801c6a1c311f3124eecb7b6772e6c5db6
Ckyre <ckyre.game@gmail.com>
2020-11-25 10:24:02 +0100

    Edited style.css to add ytb class

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 // Variables
-const aboutHtml = "<h1>A propos</h1><p>Bienvenue ! Je m’appelle Gabriel, j'ai 18 ans et après avoir obtenu un bac S, je suis actuellement en première année du DUT MMI à Bordeaux.</p><p>Ma passion pour le développement de jeux m’a permis d’acquérir de nombreuses compétences, de la programmation orientée objet, au graphisme en passant par le game design et le développement web. Je vous propose de découvrir les compétences que j’ai à vous offrir à travers mes projets !</p><p>Je ne connais pas encore la place que j’aimerais occuper dans la longue chaine de développement d’un jeu vidéo. Un <strong>stage dans un studio de développement de jeux vidéo</strong> me permettrait de me confronter à la réalité de l’industrie et je pourrais vous apporter ma motivation. Si vous êtes intéressé <a onclick='ShowContent($(\".nav-link:not(:first-child)\").get(0));'>contactez-moi ici</a> !</p>";
-const contactHtml = "<h1>Contact</h1><p>Si mon profil vous intéresse, n'hésitez pas à me contacter par <a href=\"mailto:gabriel.rouleau@mmibordeaux.com\" class=\"black\">mail</a> ou via <a href=\"https://www.linkedin.com/in/gabriel-rouleau-a9b7671b7/\" class=\"black\">LinkedIn</a> ! Vous pouvez aussi voir les jeux que j'ai conçu sur <a href=\"https://ckyre.itch.io/\" class=\"black\">ma page itch.io</a>.</p><p>Bordeaux, France<br><a href=\"mailto:gabriel.rouleau@mmibordeaux.com\">gabriel.rouleau@mmibordeaux.com</a></p><div class=\"inline\"><a href=\"https://ckyre.itch.io\"><img src=\"assets/images/itchio.svg\" alt=\"\"></a><a href=\"https://www.linkedin.com/in/gabriel-rouleau-a9b7671b7/\"><img src=\"assets/images/linkedin.svg\" alt=\"\"></a></div>";
+const aboutHtml = "<h1>A propos</h1><p>Bienvenue ! Je m’appelle Gabriel, j'ai 18 ans et après avoir obtenu un bac S, je suis actuellement en première année du DUT MMI à Bordeaux.</p><p>Ma passion, pour le développement de jeux vidéo m’a permis d’acquérir de nombreuses compétences, de la programmation orientée objet au graphisme en passant par le game design et le développement web. Je vous propose de découvrir les compétences que j’ai à vous offrir à travers mes projets !</p><p>Je ne connais pas encore la place que j’aimerais occuper dans la longue chaîne de développement d’un jeu vidéo. <strong>Un stage dans un studio de développement de jeux vidéo</strong> me permettrait de me confronter à la réalité de l’industrie et je pourrais vous apporter ma motivation. Si vous êtes intéressé, <a onclick='ShowContent($(\".nav-link:not(:first-child)\").get(0));'>contactez-moi ici</a>.</p>";
+const contactHtml = "<h1>Contact</h1><p>Si mon profil vous intéresse, n'hésitez pas à me contacter par <a href=\"mailto:gabriel.rouleau@mmibordeaux.com\" class=\"black\">mail</a> ou via <a href=\"https://www.linkedin.com/in/gabriel-rouleau-a9b7671b7/\" class=\"black\">LinkedIn</a>. Vous pouvez aussi voir les jeux que j'ai conçu sur <a href=\"https://ckyre.itch.io/\" class=\"black\">ma page itch.io</a>.</p><p>Bordeaux, France<br><a href=\"mailto:gabriel.rouleau@mmibordeaux.com\">gabriel.rouleau@mmibordeaux.com</a></p><div class=\"inline\"><a href=\"https://ckyre.itch.io\"><img src=\"assets/images/itchio.svg\" alt=\"\"></a><a href=\"https://www.linkedin.com/in/gabriel-rouleau-a9b7671b7/\"><img src=\"assets/images/linkedin.svg\" alt=\"\"></a></div>";
 var canShowContent = true;
 
 // On page was loaded
 window.addEventListener("load", function () {
    setTimeout(function () {
-        $(".bg-image").css("opacity", "0.05");
+        $(".bg-image").css("opacity", "0.03");
         $("header").css("opacity", "1");
         $(".face-img").css("opacity", "1");
 
@@ -26,7 +26,7 @@ window.addEventListener("load", function () {
                 }, 150 * index);       
             }
         }, 500);
-    }, 1500);
+    }, 700);
 });
 
 // Update left panel
