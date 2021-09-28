9289657f3a2df7703278cfecec8c4b108fd09f19
Ckyre <ckyre.game@gmail.com>
2021-09-28 16:43:15 +0200

    Open project animation

--- articles.js
+++ articles.js
@@ -39,6 +39,8 @@ a1 = new Article("Refonte de site", `
     nous avons effectué la refonte du site internet du garage bordelais Speed Moto Services. 
     Vous trouverez <a href="https://speed-motos-service.netlify.app/">notre version du site ici</a>.</p>
 
+    <img src="assets/images/projects/speed-moto-services-3.gif">
+
     <h3>Webdesign</h3>
     <p>Nous avons donc commencé par lister les problèmes de conception de l'<a href="https://www.speedmotoservices.com/">ancienne version</a> 
     et réorganiser la hiérarchie du site pour rendre l’expérience utilisateur meilleure. 
@@ -47,8 +49,17 @@ a1 = new Article("Refonte de site", `
     Je me suis occupé du design et de l’intégration de la boutique et de la page contact ainsi 
     qu'une partie de l’intégration de la page principale.</p>
 
+    <img src="assets/images/projects/speed-moto-services-2.png">
+
     <p>Nous avons ensuite travaillé sur le design des pages avec Adobe XD. 
     L'objectif étant de garder un ensemble cohérent. Il a donc fallu communiquer pour se mettre 
     d’accord sur la couleur d’interaction ou les typographies par exemple.</p>
+
+    <h3>Integration</h3>
+    <p>Après la semaine consacrée au design, nous avons effectué l’intégration avec les 
+    langages HTML, CSS et JS ainsi que la librairie P5 pour les éléments 3D.</p>
+
+    <img src="assets/images/projects/speed-moto-services-1.png">
+
 `);
 articlesDB.push(a1);
--- script.js
+++ script.js
@@ -21,18 +21,47 @@ const landingSection = document.getElementById("landing-section");
 const articleSection = document.getElementById("article-section");
 const articleTitle = document.getElementById("article-title");
 const articleContent = document.getElementById("article-content");
+const leftContainer = document.getElementById("left");
+var openProject = -1;
 
 function OpenArticle (id)
 {
-    landingSection.classList.add("unactive");
-    articleSection.classList.remove("unactive");
+    if(id != openProject)
+    {
+        articleTitle.innerHTML = articlesDB[id].Title;
+        articleContent.innerHTML = articlesDB[id].Content;
 
-    articleTitle.innerHTML = articlesDB[id].Title;
-    articleContent.innerHTML = articlesDB[id].Content;
+        // Fade animation
+        landingSection.style.opacity = "0";
+        setTimeout(function(){
+            landingSection.style.display = "none";
+            
+            articleSection.style.display = "block";
+            setTimeout(function() {
+                articleSection.style.opacity = "1";
+            }, 100);
+        }, 200);
+
+        leftContainer.scrollTop = 0;
+        openProject = id;
+    }
 }
 
 function CloseArticle()
 {
-    landingSection.classList.remove("unactive");
-    articleSection.classList.add("unactive");
+    if(openProject >= 0) 
+    {
+        // Fade animation
+        articleSection.style.opacity = "0";
+        setTimeout(function(){
+            articleSection.style.display = "none";
+            
+            landingSection.style.display = "block";
+            setTimeout(function() {
+                landingSection.style.opacity = "1";
+            }, 100);
+        }, 200);
+
+        openProject = -1;
+    }
 }
