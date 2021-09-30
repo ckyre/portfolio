12055e02b58315f473acdc2fc72ede85d9ddd22d
Ckyre <ckyre.game@gmail.com>
2021-09-30 10:53:15 +0200

    Projects contents

--- README.md
+++ README.md
@@ -6,5 +6,5 @@ Mon portfolio (https://gabrielrouleau.fr)
 
 1. Add hover effect on landing image
 2. Habiller la section landing
-3. Customiser une animation de scroll
+3. Customiser une animation de scroooooll
 4. Rédaction du contenu
--- articles.js
+++ articles.js
@@ -27,90 +27,172 @@ articlesDB.push(new Article(
         </div>
     </div>
 
-    <p>Dans le cadre d’un atelier du DUT MMI à Bordeaux, 
-    nous avons effectué la refonte du site internet du garage bordelais Speed Moto Services. 
-    Vous trouverez <a href="https://speed-motos-service.netlify.app/">notre version du site ici</a>.</p>
-
-    <img src="assets/images/projects/speed-moto-services-3.gif">
-
-    <h3>Webdesign</h3>
-    <p>Nous avons donc commencé par lister les problèmes de conception de l'<a href="https://www.speedmotoservices.com/">ancienne version</a> 
-    et réorganiser la hiérarchie du site pour rendre l’expérience utilisateur meilleure. 
-    Nous avons opté pour un découpage simple en 3 pages : la boutique, la page contact et enfin, 
-    page principale qui permet de se rendre rapidement sur les deux autres pages. 
-    Je me suis occupé du design et de l’intégration de la boutique et de la page contact ainsi 
-    qu'une partie de l’intégration de la page principale.</p>
-
-    <img src="assets/images/projects/speed-moto-services-2.png">
-
-    <p>Nous avons ensuite travaillé sur le design des pages avec Adobe XD. 
-    L'objectif étant de garder un ensemble cohérent. Il a donc fallu communiquer pour se mettre 
-    d’accord sur la couleur d’interaction ou les typographies par exemple.</p>
-
-    <h3>Integration</h3>
-    <p>Après la semaine consacrée au design, nous avons effectué l’intégration avec les 
-    langages HTML, CSS et JS ainsi que la librairie P5 pour les éléments 3D.</p>
-
-    <img src="assets/images/projects/speed-moto-services-1.png">
+    <p>Description du projet ici...</p>
 `));
 
 /****************** 2 **********************/
 articlesDB.push(new Article(
-    "222Refonte de site", `
+    "Minecraft OpenGL", `
+    <div class="article-properties">
+        <div class="flex space-between">
+            <div class="article-prop-col">
+                <p class="title">Taille de l'équipe</p>
+                <p>Seul</p>
+                <p class="title">Rôles</p>
+                <ul>
+                    <li>Programmation du Renderer</li>
+                    <li>Création du générateur de terrain</li>
+                </ul>
+            </div>
+            <div class="article-prop-col">
+                <p class="title">Outils</p>
+                <ul>
+                    <li>VS Community</li>
+                    <li>Git</li>
+                </ul>
+                <p class="title">Date</p>
+                <p>Janvier 2021</p>
+            </div>
+        </div>
+    </div>
+
+    <p>Pour mieux comprendre les outils que j’utilise au quotidien tel que Unity et 
+    Blender j’ai décidé de créer un petit moteur de jeu voxel avec l’API OpenGL. 
+    Je travaille encore sur le moteur mais je souhaitais tout de même documenter 
+    ma progression ici.
+
+    <p>Je me suis tout d’abord documenté pour comprendre le pipeline à mettre en place 
+    afin d’afficher mon premier triangle. Après avoir créé un tapon de sommets, définit 
+    un layout pour ce dernier et compilé un vertex et fragment shader basique j’ai pu 
+    effectuer mon premier draw call et afficher un triangle :</p>
+
+    <img src="assets/images/projects/premier-triangle.png"/>
+
+    <h3>Renderer</h3>
+
+    <p>Ensuite, à l’aide des bibliothèques ASSIMP et stbi_image j’ai appris à 
+    charger des fichiers .obj et à appliquer des textures :</p>
+
+    <img src="assets/images/projects/chargment-modele-texture.png"/>
+
+    <p>Grace au site <a href="http://www.opengl-tutorial.org/fr/beginners-tutorials/
+    tutorial-8-basic-shading/">opengl-tutorial.org</a> j’ai ensuite découvert la 
+    programmation de shaders en GLSL.</p>
+
+    <img src="assets/images/projects/shader-point-light.png"/>
+
+    <h3>Génération du terrain</h3>
+
+    <p>Une fois le Renderer organisé et découpé en plusieurs classes j’ai commencé 
+    la logique de génération du terrain par la création de mon premier chunk (ensemble de blocs). Le plus compliqué a été d’optimiser le chunk en affichant uniquement les faces visibles par le joueur.</p>
+
+    <img src="assets/images/projects/premier-chunk.png"/>
+
+    <p>J’ai ensuite mis en place le chargement et déchargement des chunks pour 
+    rendre le monde infini :</p>
+
+    <img src="assets/images/projects/generation-infinie.png"/>
+
+    <p>Enfin, pour obtenir un terrain convainquant j’ai utilisé le bruit Simplex 
+    afin de générer une height map. Pour rendre le terrain plus détaillé j’ai 
+    appliqué 8 couches de bruit (octaves).</p>
+
+    <img src="assets/images/projects/exemple-octaves.png"/>
+
+    <p>Voilà donc l’état actuel du terrain :</p>
+
+    <img src="assets/images/projects/height-map.png"/>
+
+    <p>Je travaille actuellement sur des modifications du générateur de terrain 
+    afin d’utiliser différents blocs dans un chunk.</p>
+`));
+
+
+/****************** 3 **********************/
+articlesDB.push(new Article(
+    "Jeu avec une équipe grecque", `
     <div class="article-properties">
         <div class="flex space-between">
             <div class="article-prop-col">
                 <p class="title">Taille de l'équipe</p>
                 <p>5</p>
                 <p class="title">Durée</p>
-                <p>2 semaines</p>
+                <p>6 mois</p>
                 <p class="title">Date</p>
-                <p>Octobre 2020</p>
+                <p>Septembre 2019</p>
             </div>
             <div class="article-prop-col">
                 <p class="title">Rôles</p>
                 <ul>
-                    <li>Webdesign</li>
-                    <li>Front-end developpement</li>
+                    <li>Programmation IA</li>
+                    <li>Programmation gameplay</li>
                 </ul>
                 <p class="title">Outils</p>
                 <ul>
-                    <li>Adobe XD</li>
-                    <li>VS Code</li>
-                    <li>Netlify</li>
+                    <li>Unity</li>
+                    <li>Unity Collab</li>
+                    <li>VS Community</li>
                 </ul>
             </div>
         </div>
     </div>
 
-    <p>Dans le cadre d’un atelier du DUT MMI à Bordeaux, 
-    nous avons effectué la refonte du site internet du garage bordelais Speed Moto Services. 
-    Vous trouverez <a href="https://speed-motos-service.netlify.app/">notre version du site ici</a>.</p>
+    <p>Souhaitant me lancer dans un projet plus conséquent et travailler en équipe, 
+    j'ai postulé à une annonce sur un forum et rejoint une équipe de développeurs 
+    indépendants grecs. J’ai travaillé en tant que programmeur sur leur projet de RPG 
+    sur Unity, projet qui est actuellement arrêté. Il y avait un Game designer, 
+    deux artistes et nous étions deux programmeurs.</p>
 
-    <img src="assets/images/projects/speed-moto-services-3.gif">
+    <img src="assets/images/projects/mephis.jpg" />
 
-    <h3>Webdesign</h3>
-    <p>Nous avons donc commencé par lister les problèmes de conception de l'<a href="https://www.speedmotoservices.com/">ancienne version</a> 
-    et réorganiser la hiérarchie du site pour rendre l’expérience utilisateur meilleure. 
-    Nous avons opté pour un découpage simple en 3 pages : la boutique, la page contact et enfin, 
-    page principale qui permet de se rendre rapidement sur les deux autres pages. 
-    Je me suis occupé du design et de l’intégration de la boutique et de la page contact ainsi 
-    qu'une partie de l’intégration de la page principale.</p>
+    <h3>Le jeu</h3>
 
-    <img src="assets/images/projects/speed-moto-services-2.png">
+    <p>Ce jeu 3D à la troisième personne prenait place dans un monde médiéval 
+    fantastique. Son Level design était inspiré par celui de Dark Souls qui offre 
+    de nombreux chemins alternatifs. La progression était elle aussi non-linéaire 
+    et se faisait via l’acquisition de compétences permettant au joueur de personnaliser 
+    son style de jeu.</p>
 
-    <p>Nous avons ensuite travaillé sur le design des pages avec Adobe XD. 
-    L'objectif étant de garder un ensemble cohérent. Il a donc fallu communiquer pour se mettre 
-    d’accord sur la couleur d’interaction ou les typographies par exemple.</p>
+    <img src="assets/images/projects/mephis_in_corridor.jpg" />
 
-    <h3>Integration</h3>
-    <p>Après la semaine consacrée au design, nous avons effectué l’intégration avec les 
-    langages HTML, CSS et JS ainsi que la librairie P5 pour les éléments 3D.</p>
+    <h3>Mes missions</h3>
 
-    <img src="assets/images/projects/speed-moto-services-1.png">
+    <p>A mon arrivée dans l’équipe j’ai mis en place <strong>l’intelligence 
+    artificielle des ennemis</strong>.</p>
+
+    <p>Je me suis d’abord occupé du pathfinding géré par une classe commune à tous 
+    les NPC (passifs ou agressifs) du jeu. Cette dernière permet par exemple aux 
+    personnages de se rendre à un endroit ou de fuir un objet grâce au composant 
+    NavMeshAgent d'Unity.</p>
+
+    <p>Je me suis ensuite occupé de la classe commune aux ennemis, en mettant en 
+    place un finite-state machine afin de pouvoir décliner le code pour chaque ennemi 
+    plus facilement. Le joueur pouvant adopter plusieurs façons de jouer, les ennemis 
+    allaient donc avoir de nombreuses réactions différentes selon les types d’ennemis. 
+    Vous trouverez ci-dessous une démonstration de certains comportements des ennemis 
+    en infiltration :</p>
+
+    <iframe id="ytb" width="560" height="315" src="https://www.youtube.com/embed/o-CC24HkcXo" 
+    frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; 
+    picture-in-picture" allowfullscreen></iframe>
+
+    <h3>Difficultés rencontrées</h3>
+
+    <p>La communication a parfois été compliqué étant donné que nos réunions hebdomadaires 
+    étaient en anglais mais, mes coéquipiers ont toujours pris en compte mes difficultés 
+    et j’ai énormément progressé depuis cette expérience. De plus, j’ai intégré l’équipe 
+    avec des bases en C# mais, j’ai dû m’adapter assez vite et notamment apprendre à mettre 
+    en place un système de finite-state machine pour les ennemis.</p>
+
+    <img src="assets/images/projects/solsgardfinal.jpg" />
+
+    <p>Je vous propose d’écouter la musique qu’un musicien a produit pour ce jeu :<br><br>
+    <audio controls src="assets/sounds/battle-wip2.mp3"> Your browser does not support 
+    the audio element.</audio></p>
+
 `));
 
-/****************** 3 **********************/
+/****************** 4 **********************/
 articlesDB.push(new Article(
     "Atelier jeux vidéo MMI", `
     <div class="article-properties">
@@ -139,7 +221,7 @@ articlesDB.push(new Article(
         </div>
     </div>
 
-    <iframe width="560" height="315" src="https://www.youtube.com/embed/ZXLYm_i6tGw" title="YouTube video player" 
+    <iframe id="ytb" width="560" height="315" src="https://www.youtube.com/embed/ZXLYm_i6tGw" title="YouTube video player" 
     frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
     allowfullscreen></iframe>
 
@@ -147,10 +229,16 @@ articlesDB.push(new Article(
     Unity. Le rendu final de ce projet est la vidéo ci-dessus.</p>
 
     <h3>Le jeu</h3>
+
     <p>Ce jeu est un beat them all 2D où le joueur doit éliminer des vagues d'ennemis avec un boomerang 
     afin de récolter de l’argent. Cet argent peut être dépensé pour améliorer l'équipement, acheter de la 
     vie ou passer au niveau suivant.</p>
 
     <img src="assets/images/projects/akagani-gameplay.gif">
+    
+    <iframe src="https://itch.io/embed/817421?bg_color=FFFCF9&amp;fg_color=352D39&amp;
+    link_color=FF2E2E&amp;border_color=feebeb" width="552" height="167" frameborder="0">
+    <a href="https://fikacorp.itch.io/akagani">Akagani by Fikacorp, MerlinWasTaken, Niweb, 
+    Kayo SILVA, kodypistol, Gabriel ROULEAU</a></iframe>
 
 `));
